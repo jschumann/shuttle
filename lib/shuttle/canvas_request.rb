8eda65f7f43ebc34b819501c410f277577284235
James Schumann <jschumann@gmail.com>
2012-08-22 09:09:09 -0400

    made Canvas::Request survive some http errors

--- lib/shuttle/canvas_request.rb
+++ lib/shuttle/canvas_request.rb
@@ -7,29 +7,54 @@ class Canvas::API::Request
   attr_accessor :body
 
   def fire!
-    uri = URI.parse("#{Canvas::API::BaseURI}#{self.endpoint}")
-    http = Net::HTTP.new(uri.host, uri.port)
+    request_counter = 0
+    begin
+      uri = URI.parse("#{Canvas::API::BaseURI}#{self.endpoint}")
+      http = Net::HTTP.new(uri.host, uri.port)
 
-    case self.method
-    when 'GET'
-      request = Net::HTTP::Get.new(uri.path)
-    when 'POST'
-      request = Net::HTTP::Post.new(uri.path)
-      request.body = self.body || ''
-    when 'PUT'
-      request = Net::HTTP::Put.new(uri.path)
-      request.body = self.body || ''
-    when 'DELETE'
-      request = Net::HTTP::Delete.new(uri.path)
+      case self.method
+      when 'GET'
+        request = Net::HTTP::Get.new(uri.path)
+      when 'POST'
+        request = Net::HTTP::Post.new(uri.path)
+        request.body = self.body || ''
+      when 'PUT'
+        request = Net::HTTP::Put.new(uri.path)
+        request.body = self.body || ''
+      when 'DELETE'
+        request = Net::HTTP::Delete.new(uri.path)
+      end
+      request["Authorization"] = "Bearer #{Canvas::API::TOKEN}"
+      request["Content-Type"] = "application/json"
+      response = http.request(request)
+      code = response.code
+      body = JSON.parse(response.body)
+      return Array[code, body]
+    rescue EOFError
+      puts "EOFError encountered. Wait 30 seconds for the dust to clear..."
+      sleep 30
+      puts "Retry request..."
+      if request_counter < 6
+        request_counter += 1
+        puts "Attempt #{request_counter}"
+        retry
+      else
+        puts "FAILED REQUEST #{request_counter} TIMES"
+        counter = 0
+      end
+    rescue ECONNRESET
+      puts "ECONNRESET encountered. Wait 30 seconds for the dust to clear..."
+      sleep 30
+      puts "Retry request..."
+      if request_counter < 6
+        request_counter += 1
+        puts "Attempt #{request_counter}"
+        retry
+      else
+        puts "FAILED REQUEST #{request_counter} TIMES"
+        counter = 0
+      end
     end
-    request["Authorization"] = "Bearer #{Canvas::API::TOKEN}"
-    request["Content-Type"] = "application/json"
-
-    response = http.request(request)
-    
-    code = response.code
-    body = JSON.parse(response.body)
-    return Array[code, body]
   end
 
 #      if response.code == 200
